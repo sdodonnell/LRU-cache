6f78627ac8f565bfdd364d628f86c1bc16a80dec
Sam <sam.d.odonnell@gmail.com>
2019-04-19 18:21:16 -0700

    Add linked list methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -13,13 +13,27 @@ class Node
     "#{@key}: #{@val}"
   end
 
-  def remove
-    
+  def remove      
+    if !@next && @prev
+      @prev.next = nil
+    else 
+      @prev.next = @next
+      @next.prev = @prev
+    end
   end
 end
 
 class LinkedList
+  include Enumerable
+
+  attr_reader :head, :tail, :size
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
+    @size = 2
   end
 
   def [](i)
@@ -28,21 +42,59 @@ class LinkedList
   end
 
   def first
+    if self.head.next == self.tail
+      nil
+    else
+      self.head.next
+    end
   end
 
   def last
+    if self.tail.prev == self.head
+      nil
+    else
+      self.tail.prev
+    end
   end
 
   def empty?
+    self.size > 2
   end
 
   def get(key)
+    # node = self.head
+    # until node.key == key || node.next == nil
+    #   node = node.next
+    # end
+    # node.val
   end
 
   def include?(key)
+    node = self.head
+    until node.next == self.tail
+      if node.key == key
+        return true
+      end
+      node = node.next
+    end
+    return false
   end
 
   def append(key, val)
+    curr_node = self.head.next
+
+    if !curr_node.val || val < curr_node.val
+      curr_node.next = Node.new(key, val)
+      curr_node.next.next = curr_node
+      return
+    end
+    
+    until !curr_node.val || val < curr_node.val
+      curr_node = curr_node.next
+    end
+
+    curr_node.next = Node.new(key, val)
+    curr_node.next.next = curr_node
   end
 
   def update(key, val)
