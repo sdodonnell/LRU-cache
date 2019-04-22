13dd915e2dda122127075032793324826e3e1e04
Sam <sam.d.odonnell@gmail.com>
2019-04-22 09:32:38 -0700

    Add LRU cache methods

--- lib/lru_cache.rb
+++ lib/lru_cache.rb
@@ -14,6 +14,12 @@ class LRUCache
   end
 
   def get(key)
+    if @map.get(key)
+      node = @map.get(key)
+      update_node!(node)
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -23,13 +29,23 @@ class LRUCache
   private
 
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    @map[key] = @store.append(key, val)
+
+    eject! if count > @max
+    val
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    node.remove
+    @map[node.key] = @store.append(node.key, node.val)
+    node.val
   end
 
   def eject!
+    node = @store.first
+    node.remove
+    @map.delete(node.key)
+    nil
   end
 end
